7f92d5f322175ecacc686554ed4e446b0cac6aeb
Brijeshkumar Patel <brijeshpatelcan@gmail.com>
2023-10-30 22:44:37 -0500

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://book-store-server-uze2.onrender.com",
     },
   },
   plugins: [vue()],
